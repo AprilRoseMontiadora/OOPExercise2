f02cf96ac110dafd8caf2da9dbb601be635443db
AprilRoseMontiadora <116123112+AprilRoseMontiadora@users.noreply.github.com>
2022-10-31 13:32:41 +0800

    Update Animal.java

--- Animal.java
+++ Animal.java
@@ -1,3 +1,28 @@
-public class Animal
-{
+public abstract class  Animal { 
+
+	protected String name; 
+	protected double weight;
+
+	public Animal (String name, double weight) {
+		this.name=name; 
+		this.weight=weight;
+	}
+    public abstract String makeSound();
+
+	public String getName() {
+		return name;
+	}
+	public void setName (String name) {
+		this.name=name;
+	}
+	public double getWeight() {
+		return weight;
+	}
+	public void setWeight (double weight) {
+		this.weight=weight;
+	}
+
+
+
+
 }
