b968a0f077f2cc05c285970870544ee079350f7f
AprilRoseMontiadora <116123112+AprilRoseMontiadora@users.noreply.github.com>
2022-10-31 13:34:17 +0800

    Update Sheep.java

--- Sheep.java
+++ Sheep.java
@@ -1,3 +1,20 @@
-public class Sheep
-{
+public class Sheep extends Animal{
+	private String farm;
+
+	public Sheep (String name, double weight, String farm){
+		super(name,weight);
+		this.farm=farm;
+	}
+
+	@Override
+	public String makeSound() {
+		return "Baaa!Baaa!";
+	}
+	public String getBreed() {
+		return farm;
+	}
+
+	public void setFarm(String farm){
+		this.farm=farm;
+	}
 }
