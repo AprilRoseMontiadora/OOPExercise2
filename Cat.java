90cfcac4033d8e710f68092f7ce44c060c85613f
AprilRoseMontiadora <116123112+AprilRoseMontiadora@users.noreply.github.com>
2022-10-31 13:33:28 +0800

    Update Cat.java

--- Cat.java
+++ Cat.java
@@ -1,3 +1,20 @@
-public class Cat
-{
+public class Cat extends Animal{
+	private String breed;
+
+	public Cat (String name, double weight, String breed){
+		super(name,weight);
+		this.breed=breed;
+	}
+
+	@Override
+	public String makeSound() {
+		return "Meow!Meow!";
+	}
+	public String getBreed() {
+		return breed;
+	}
+
+	public void setBreed(String breed){
+		this.breed=breed;
+	}
 }
