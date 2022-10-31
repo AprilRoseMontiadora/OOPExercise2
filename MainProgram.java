73c4dd4d7be607af857f1b9504d406bdef85687f
AprilRoseMontiadora <116123112+AprilRoseMontiadora@users.noreply.github.com>
2022-10-31 13:30:05 +0800

    Update MainProgram.java

--- MainProgram.java
+++ MainProgram.java
@@ -1,3 +1,13 @@
-public class MainProgram
-{
+public class MainProgram{
+	public static void main(String[] args) {
+		Animal animal;
+		Cat cat = new Cat ("Snow",7, " Siberian");
+		Sheep sheep=new Sheep("Bob", 1, "Harri"); 
+
+		animal=cat;
+		System.out.println(animal.makeSound()+ " " + "My name is "+ animal.getName());
+
+		animal=sheep;
+		System.out.println(animal.makeSound()+ " " + "My name is "+ animal.getName());
+	}
 }
